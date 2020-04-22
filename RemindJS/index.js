e52b625fe141ed1b4e30a70eb3a4188526619b4b
JeonHyungJin <qpdjgjsl@gmail.com>
2020-04-22 21:40:43 +0900

    Data Type

--- RemindJS/index.js
+++ RemindJS/index.js
@@ -8,3 +8,18 @@ console.log(b);
 const c = 221;
 //Can`t use c again like c = 5;
 
+//Data Type
+//String Type
+const what = "Nicolas";
+console.log(what);
+
+//Boolean Type
+const wat = true;
+console.log(wat);
+
+//Number Type
+const wat2 = 666;
+
+//Float Type
+const wat3 = 66.1;
+
