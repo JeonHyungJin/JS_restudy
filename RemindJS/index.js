717eed42a2bee7ae3210ba18b37605748e523587
JeonHyungJin <qpdjgjsl@gmail.com>
2020-04-22 22:13:32 +0900

    Array, Object

--- RemindJS/index.js
+++ RemindJS/index.js
@@ -23,3 +23,32 @@ const wat2 = 666;
 //Float Type
 const wat3 = 66.1;
 
+//Array
+const mon = "Mon";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "Thu";
+const fri = "Fri";
+
+console.log(mon, tue, wed, thu, fri);
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+console.log(daysOfWeek);
+
+//Array index start from 0
+console.log(daysOfWeek[2]);
+
+//Object
+const MyInfo = {
+    name: "Hyungjin",
+    age: 27,
+    gender: "Male",
+    favMovies: ["Along the gods", "LOTR", "Oldboy"],
+    favFood: [{ name: "Kimchi", fatty: false }, { name: "Cheese burger", fatty: true }]
+};
+
+console.log(MyInfo);
+console.log(MyInfo.age);
+
+MyInfo.age = 28;
+console.log(MyInfo.age);
