5ff2cf6d910db16dfac6694ca07172e627e78a5d
JeonHyungJin <qpdjgjsl@gmail.com>
2020-04-23 01:27:28 +0900

    Dom if-else

--- RemindJS/index.js
+++ RemindJS/index.js
@@ -96,7 +96,7 @@ console.log(plus);
 //DOM Functions
 const title = document.getElementById("title");
 title.innerHTML = "Hi!, From JS";
-title.style.color = 'red';
+//title.style.color = 'red';
 document.title = "I own you now";
 
 //Event
@@ -105,12 +105,6 @@ function handleResize() {
 }
 
 window.addEventListener("resize", handleResize);
-
-function handleClick() {
-    title.style.color = "blue";
-}
-
-title.addEventListener("click", handleClick);
 
 //if-else
 if (10 > 5) {
@@ -126,3 +120,22 @@ if (20 > 5 && "nico" === "nico") {
 }
 
 //Dom if-else function
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+}
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+
+init();
